6a3be7b389ad42cf2820c52f471b3bfd4e9fe2dc
Kay <info@beckmann-md.de>
2026-06-26 19:43:39 +0200

    chore: CI-Stub (Woodpecker), l10n nach lib/l10n korrigiert

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:void_trader/l10n/app_localizations.dart';
 
 class VoidTraderApp extends StatelessWidget {
   const VoidTraderApp({super.key});
