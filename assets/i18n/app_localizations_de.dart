8b2ae6058fd07923e113c887106d18346f7ff1b8
Kay <info@beckmann-md.de>
2026-06-26 19:42:45 +0200

    chore: Projektstruktur und Lokalisierungs-Setup (l10n, ARB DE/EN)

---
+++ assets/i18n/app_localizations_de.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get appTitle => 'Void Trader';
+
+  @override
+  String get docking => 'Andocken';
+
+  @override
+  String get market => 'Markt';
+
+  @override
+  String get fleet => 'Flotte';
+
+  @override
+  String get base => 'Basis';
+
+  @override
+  String get research => 'Forschung';
+
+  @override
+  String get credits => 'Credits';
+}
---
+++ assets/i18n/app_localizations_en.dart
@@ -0,0 +1,31 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Void Trader';
+
+  @override
+  String get docking => 'Dock';
+
+  @override
+  String get market => 'Market';
+
+  @override
+  String get fleet => 'Fleet';
+
+  @override
+  String get base => 'Base';
+
+  @override
+  String get research => 'Research';
+
+  @override
+  String get credits => 'Credits';
+}
